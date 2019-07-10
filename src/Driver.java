c0a657ee0a9486aa111a7ed1d3fdf3eaf5912a34
bentonsmith <bdsmith98@gmail.com>
2019-07-09 20:50:40 -0500

    Finished problem 3

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,5 @@
 public class Driver {
     public static void main(String[] args) {
-        System.out.println(Problem3.largestPrimeFactor(600_851_475_143));
+        System.out.println(Problem3.largestPrimeFactor(Long.parseLong("600851475143")));
     }
 }
--- src/Problem3.java
+++ src/Problem3.java
@@ -1,41 +1,17 @@
 public class Problem3 {
     public static long largestPrimeFactor (long max) {
-        java.util.ArrayList<Long> numberArray= new java.util.ArrayList<>();
-        java.util.ArrayList<Boolean> isNotDone = new java.util.ArrayList<>();
+        long test = max;
 
-        // Populating list of numbers and booleans
-        for (long i = 0; i < max - 1; i++) {
-            numberArray.get(i) = + 2;
-            isNotDone[i] = true;
+        while (test % 2 == 0) {
+            test /= 2;
         }
 
-        // Finding prime numbers
-        for(long i = 0; i < max - 1; i++) {
-            if(isNotDone[i]) {
-                for(long j = i; j < max - 1; j += numberArray[i]) {
-                    isNotDone[j] = false;
-                }
-                isNotDone[i] = true;
+        for(long i = 3; i < test; i++) {
+            while (test % i == 0 && i < test) {
+                test /= i;
             }
         }
 
-        // Getting an ArrayList of prime numbers
-        java.util.ArrayList<Integer> primeNumbers = new java.util.ArrayList<>();
-
-        for(long i = 0; i < max - 2; i++) {
-            if(isNotDone[i]) {
-                primeNumbers.add(numberArray[i]);
-            }
-        }
-
-        long maxVal = 0;
-
-        // Finding largest prime factor
-        for (long number : primeNumbers) {
-            if (max % number == 0) {
-                maxVal = number;
-            }
-        }
-        return maxVal;
+        return test;
     }
 }
